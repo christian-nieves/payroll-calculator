c537f34962844170efff5c0e966de615f8db7529
christian-nieves <thechristiannieves@gmail.com>
2026-04-08 15:32:38 -0400

    Name and Gross Pay Final Commit

--- src/main/java/com/pluralsight/PayrollCalc.java
+++ src/main/java/com/pluralsight/PayrollCalc.java
@@ -1,4 +1,23 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class PayrollCalc {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("What is your name?: ");
+        String name = myScanner.nextLine();
+
+        System.out.print("How many hours did you work?: ");
+        int hoursWorked = myScanner.nextInt();
+
+        System.out.print("What is your pay rate?: ");
+        float payRate = myScanner.nextFloat();
+
+        float grossPay = payRate * hoursWorked;
+
+        System.out.printf("Name: %s, Gross Pay: $%.2f", name, grossPay);
+
+
+    }
 }
